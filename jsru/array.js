8b0f918e8154c5f91efbc810a30ef105bd3380e3
dashakim <dasha.tabala@gmail.com>
2018-02-12 00:34:25 +0200

    use reduce

---
+++ jsru/array.js
@@ -0,0 +1,34 @@
+var arr = ["Яблоко", "Апельсин", "Груша"];
+
+arr.forEach(function(item, i, arr) {
+ console.log( i + ": " + item + " (массив:" + arr + ")" );
+});
+
+var arr = [1, 2, 3, 4, 5]
+var result = arr.reduce(function(sum, current) {
+  return sum + current;
+}, 0);
+
+console.log( result ); 
+
+var arr = ["Есть", "жизнь", "на", "Марсе"];
+var arrLength = arr.map(function(names) {
+    return names.length;
+});
+
+console.log(arrLength);
+
+function getSums(arr) {
+    var result = [];
+    if (!arr.length) return result; // проверяем не пустой ли массив
+    var totalSum = arr.reduce(function(sum, item) {
+        result.push(sum);
+        return sum + item;
+    });
+    result.push(totalSum);
+    
+    return result;
+}
+
+console.log(getSums([1,2,3,4,5]));
+console.log(getSums([-2,-1,0,1]));
